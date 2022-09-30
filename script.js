b47f8719ea42176dd7804f3580fbf8c9bfcb4beb
Sheldon Fam <sheldonfam95@gmail.com>
2022-09-30 16:43:45 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-const hamburger = document.querySelector(".hamburger-menu-wrapper");
-const close = document.querySelector(".hamburger-close");
-const navMenu = document.querySelector(".main-nav");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-hamburger.addEventListener("click", () => {
-  console.log("click");
-  navMenu.classList.toggle("active");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
-
-close.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
-// document.querySelectorAll(".main_menu nav ul li a").forEach((n) =>
-//   n.addEventListener("click", () => {
-//     hamburger.classList.remove("active");
-//     navmenu.classList.remove("active");
-//   })
-// );
